c3d0ff6660096052aa55d7405d15aa2cf2c62bb8
Renuka <renuka@interleap.co>
2022-03-16 14:21:18 +0530

    feat: add functionality to get sum of two comma seperated numbers in a string

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,14 +1,14 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Calculator {
     public static int add(String numberString){
         if(numberString.isEmpty()) {
             return 0;
         }
-        String[] tokens = numberString.split(",");
-        List<Integer> numbers = Arrays.stream(tokens).map(token -> Integer.parseInt(token)).collect(Collectors.toList());
-        return numbers.stream().reduce(0,(num1,num2) -> num1+num2);
+        else{
+            if(numberString.contains(",")){
+                String[] numbers = numberString.split(",");
+                return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+            }
+            return Integer.parseInt(numberString);
+        }
     }
 }
