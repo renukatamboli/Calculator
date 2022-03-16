016668127feb990b1c4c6cc0643b465bcd3a8ef9
Renuka <renuka@interleap.co>
2022-03-16 12:56:26 +0530

    test: add test for empty string

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,3 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
 
+    @Test
+    public void shouldReturnZeroForEmptyString(){
+        assertEquals(0, Calculator.add(""));
+    }
 }
